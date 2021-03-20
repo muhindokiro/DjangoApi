530bd33c57c014df4984ea50e7f1946226fd4a27
muhindokiro <muhindokiro@gmail.com>
2021-03-20 18:06:44 +0300

    django api

---
+++ apidjango/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path,include
+from .views import article_list,article_detail,ArticleAPIView,ArticleDetails,GenericAPIView,ArticleViewSet
+from rest_framework.routers import DefaultRouter
+# from . import views
+
+router =  DefaultRouter()
+router.register('article', ArticleViewSet, basename = 'article')
+
+
+urlpatterns = [
+    # path('register/', views.registerPage),
+    # path('login/', views.loginPage),
+
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>/', include(router.urls)),
+    # path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>', ArticleDetails.as_view()),
+    path('generic/article/<int:id>/', GenericAPIView.as_view()),
+    # path('detail/<int:pk>/', article_detail),
+    
+]
